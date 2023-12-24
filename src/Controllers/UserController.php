cbd84121536bbe8ef885020da2fa05c65d980865
Riccardo Iocolano <riccardo.iocolano@gmail.com>
2023-12-24 18:51:23 +0100

    User creation implemented

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -18,6 +18,51 @@ class UserController
     {
         $user = $this->gateway->getById($id);
 
+        if ($user === false) {
+            new Response([
+                "Error" => "User not found"
+            ], 500);
+            return;
+        }
+
         new Response($user, 200);
     }
+
+    public function registerUser()
+    {
+        $body = (array) json_decode(file_get_contents("php://input"), true);
+        $validation_errors = $this->validateUser($body);
+
+        if (is_array($validation_errors)) {
+            http_response_code(422);
+            echo json_encode(["errors" => $validation_errors]);
+            die;
+        }
+
+        $body['password'] = password_hash($body['password'], PASSWORD_BCRYPT);
+
+        $id = $this->gateway->create($body);
+
+        new Response([
+            $id => "User created"
+        ], 201);
+    }
+
+    private function validateUser($data): array | false
+    {
+        $validation = Validation::getInstance();
+
+        $validation->setData($data);
+
+        $validation->field('user')->type('text')->pattern("/^[a-z0-9_-]{3,15}$/")->required();
+        $validation->field('password')->type('text')->pattern("/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/")->required();
+        $validation->field('name')->type('text')->required();
+        $validation->field('surname')->type('text')->required();
+        $validation->field('province')->type('text')->required();
+
+        return $validation->hasErrors();
+    }
 }
+
+// (user, password, name, surname, province, date_created, date_updated, role)
+// test cli: 
--- src/Gateways/UserGateway.php
+++ src/Gateways/UserGateway.php
@@ -34,4 +34,25 @@ class UserGateway
 
         return $result;
     }
+
+    public function create(array $data): string
+    {
+        $sql = "INSERT INTO users (user, password, name, surname, province, date_created, date_updated, role)
+                VALUES (:user, :password, :name, :surname, :province, :date_created, :date_updated, :role)";
+
+        $stmt = $this->db->prepare($sql);
+
+        $stmt->bindValue(":user", $data["user"], PDO::PARAM_STR);
+        $stmt->bindValue(":password", $data["password"], PDO::PARAM_STR);
+        $stmt->bindValue(":name", $data["name"], PDO::PARAM_STR);
+        $stmt->bindValue(":surname", $data["surname"], PDO::PARAM_STR);
+        $stmt->bindValue(":province", $data["province"], PDO::PARAM_STR);
+        $stmt->bindValue(":date_created", date("Y/m/d"));
+        $stmt->bindValue(":date_updated", null);
+        $stmt->bindValue(":role", 2);
+
+        $stmt->execute();
+
+        return $this->db->lastInsertId();
+    }
 }
