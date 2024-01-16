5ef1ea9cb911fc93515adcef8029f3a4b39ee91f
Riccardo Iocolano <riccardo.iocolano@gmail.com>
2024-01-16 09:49:23 +0100

    Added datetime filter to validation class

--- src/Validation/FilterTrait.php
+++ src/Validation/FilterTrait.php
@@ -1,6 +1,4 @@
 <?php
-
-
 trait FilterTrait
 {
     protected function text($value): string | bool
@@ -21,5 +19,12 @@ trait FilterTrait
     protected function list($value)
     {
         return filter_var_array($value);
+    }
+
+    protected function datetime($value)
+    {
+        $format = "Y-m-d H:i:s";
+        $date_time = DateTime::createFromFormat($format, $value);
+        return $date_time && $date_time->format($format) === $value;
     }
 }
